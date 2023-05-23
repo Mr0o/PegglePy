58eaa10e3e2e0202c1627605819bccca2dbe3212
Owen Smith <owen.smith1201@gmail.com>
2023-05-23 01:20:12 -0400

    Added bounds checking, save check and warning message

--- editor.py
+++ editor.py
@@ -17,6 +17,8 @@ from local.peg import Peg
 
 from local.misc import createStaticImage, updateStaticImage
 
+from local.trigger_events import TimedEvent
+
 ##### pygame stuff #####
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))  # display surface
@@ -38,6 +40,21 @@ backgroundImg =  pygame.transform.scale(backgroundImg, (WIDTH, HEIGHT))
 ballCountFont = pygame.font.Font("resources/fonts/Evogria.otf", 30)
 infoFont = pygame.font.Font("resources/fonts/Evogria.otf", 16)
 debugFont = pygame.font.Font("resources/fonts/Evogria.otf", 12)
+helpFont = pygame.font.Font("resources/fonts/Evogria.otf", 14)
+warnFont = pygame.font.Font("resources/fonts/Evogria.otf", 25)
+
+# pegs img with transparency (for mouse hover and invalid peg placement (red))
+tempPeg = Peg(0, 0)
+transparentPegImg = tempPeg.pegImg.copy()
+# set alpha to 50
+transparentPegImg.fill((255, 255, 255, 50), None, pygame.BLEND_RGBA_MULT)
+
+# create a second copy of the peg image, but with a red tint
+invalidPegImg = tempPeg.pegImg.copy()
+invalidPegImg.fill((255, 0, 0, 60), None, pygame.BLEND_RGBA_MULT)
+
+# warning timer for displaying warning messages
+warningTimer = TimedEvent()
 
 ##### drawing functions #####
 def drawCircle(x, y, rad, rgb):
@@ -54,6 +71,11 @@ skipValidPegCheck = False
 pegs : list[Peg]
 pegs = []
 
+widthBound = 35
+heightBound = 150
+
+debug = True
+
 staticImg = createStaticImage(pegs)
 
 ##### main loop #####
@@ -68,13 +90,24 @@ while True:
                 staticImg = createStaticImage(pegs)
             # save level
             if event.key == pygame.K_s:
-                saveData(pegs)
+                if len(pegs) == 0 or len(pegs) < 30:
+                    print("WARN: Level must have at least 30 pegs before it is saved...")
+                    # start the warning timer to display the message for 4 seconds
+                    warningTimer.setTimer(4)
+                else:
+                    saveData(pegs)
             # load level
             if event.key == pygame.K_l:
                 pegs = loadData()
                 staticImg = createStaticImage(pegs)
             if event.key == pygame.K_1:
                 skipValidPegCheck = not skipValidPegCheck
+            if event.key == pygame.K_p: # print all the peg positions (x,y) to the terminal
+                print("Pegs:")
+                pegsPos = []
+                for peg in pegs:
+                    pegsPos.append((peg.pos.vx, peg.pos.vy))
+                print(pegsPos)
 
     
     ##### update #####
@@ -88,10 +121,14 @@ while True:
     if mouseClicked[0]:
         validNewPegPos = True
         if not skipValidPegCheck:
-            for peg in pegs:
-                if  isBallTouchingPeg(mousePos.vx, mousePos.vy, peg.radius/6, peg.pos.vx, peg.pos.vy, peg.radius):
-                    validNewPegPos = False
-                    break
+            # check if the mouse is in a valid position to place a peg
+            if mousePos.vy > HEIGHT - heightBound or mousePos.vy < heightBound or mousePos.vx > WIDTH - widthBound or mousePos.vx < widthBound:
+                validNewPegPos = False
+            else:
+                for peg in pegs:
+                    if  isBallTouchingPeg(mousePos.vx, mousePos.vy, peg.radius/6, peg.pos.vx, peg.pos.vy, peg.radius):
+                        validNewPegPos = False
+                        break
         
         else:
             validNewPegPos = True
@@ -99,9 +136,9 @@ while True:
         
         # valid position, add peg
         if validNewPegPos:     
-            newBall = Peg(mousePos.vx, mousePos.vy)
-            pegs.append(newBall)
-            staticImg = updateStaticImage(staticImg, newBall)
+            newPeg = Peg(mousePos.vx, mousePos.vy)
+            pegs.append(newPeg)
+            staticImg = updateStaticImage(staticImg, newPeg)
     
     # if right clicked, remove peg
     elif mouseClicked[2]:
@@ -116,13 +153,58 @@ while True:
 
         if len(selectedPegs) > 0:
             staticImg = createStaticImage(pegs)
+        
+        
 
     ##### draw #####
     screen.blit(staticImg, (0, 0))
 
+    # check if the mouse is in a valid position to place a peg
+    # draw a red line across the screen to indicate that bounds
+    if mousePos.vy > HEIGHT - heightBound:
+        drawLine(0, HEIGHT-heightBound, WIDTH, HEIGHT-heightBound)
+        # draw red invalid peg
+        screen.blit(invalidPegImg, (mousePos.vx - tempPeg.posAdjust, mousePos.vy - tempPeg.posAdjust))
+    elif mousePos.vy < heightBound:
+        drawLine(0, heightBound, WIDTH, heightBound)
+        # draw red invalid peg
+        screen.blit(invalidPegImg, (mousePos.vx - tempPeg.posAdjust, mousePos.vy - tempPeg.posAdjust))
+    elif mousePos.vx > WIDTH - widthBound:
+        drawLine(WIDTH-widthBound, 0, WIDTH-widthBound, HEIGHT)
+        # draw red invalid peg
+        screen.blit(invalidPegImg, (mousePos.vx - tempPeg.posAdjust, mousePos.vy - tempPeg.posAdjust))
+    elif mousePos.vx < widthBound:
+        drawLine(widthBound, 0, widthBound, HEIGHT)
+        # draw red invalid peg
+        screen.blit(invalidPegImg, (mousePos.vx - tempPeg.posAdjust, mousePos.vy - tempPeg.posAdjust))
+    else: # its a valid position
+        # draw transparent peg
+        screen.blit(transparentPegImg, (mousePos.vx - tempPeg.posAdjust, mousePos.vy - tempPeg.posAdjust))
+
     # draw peg count text
-    pegCount = infoFont.render("Pegs : " + str(len(pegs)), False, (5, 30, 100))
+    pegCountColor = (5, 30, 100)
+    if len(pegs) >= 100:
+        pegCountColor = (120, 30, 60)
+    pegCount = infoFont.render("Pegs : " + str(len(pegs)), False, pegCountColor)
     screen.blit(pegCount, (int(WIDTH/2 - 50), 20))
+
+    # draw help text
+    helpText = helpFont.render("Left click to add peg, right click to remove peg", False, (255,255,255))
+    screen.blit(helpText, (int(WIDTH-350), 10))
+    helpText2 = helpFont.render("S = Save", False, (255,255,255))
+    screen.blit(helpText2, (int(WIDTH-150), 25))
+    helpText3 = helpFont.render("L = Load", False, (255,255,255))
+    screen.blit(helpText3, (int(WIDTH-150), 40))
+
+    # draw warning text
+    warningTimer.update()
+    if warningTimer.isActive and len(pegs) == 0:
+        warningText = warnFont.render("Cannot save empty level...", False, (255,20,25))
+        screen.blit(warningText, (int(WIDTH/2 - 150), HEIGHT/2))
+    elif warningTimer.isActive and len(pegs) > 0 and len(pegs) < 30:
+        warningText = warnFont.render("Level must have at least 30 pegs...", False, (255,20,25))
+        screen.blit(warningText, (int(WIDTH/2 - 200), HEIGHT/2))
+
 
     pygame.display.update()
     clock.tick(144)  # lock game framerate to 144 fps
