120dfdac478ea81d1e7672940d7eeafe3173a445
Owen Smith <owen.smith1201@gmail.com>
2023-06-13 17:13:42 -0400

    Added main menu music and pause menu function

--- menu.py
+++ menu.py
@@ -1,12 +1,17 @@
 import pygame
 
 from local.config import WIDTH, HEIGHT
-from local.resources import backgroundImg, menuFont, menuButtonFont
+from local.resources import backgroundImg, menuFont, menuButtonFont, menuMusicPath, buttonHoverSound, buttonClickSound
 from local.vectors import Vector
+from local.audio import playSoundPitch
 
 
 # this menu will serve as the point where the game and the editor can both be accessed
 def mainMenu(screen: pygame.Surface):
+    # play menu music
+    pygame.mixer.music.load(menuMusicPath)
+    pygame.mixer.music.play(-1)
+
     # button positions
     startButtonPos = Vector(100, 100)
     startButtonSize = Vector(100, 50)
@@ -40,7 +45,7 @@ def mainMenu(screen: pygame.Surface):
         mx, my = pygame.mouse.get_pos()
         mousePos = Vector(mx, my)
 
-        # check mouse input
+        ## check mouse input
 
         # check if mouse is over start button
         startButtoncolor = startButtoncolorOriginal
@@ -48,6 +53,7 @@ def mainMenu(screen: pygame.Surface):
             # mouse button is down
             if mouseDown:
                 selection = "start"
+                playSoundPitch(buttonClickSound)
             
             # increase the green value of the start button color
             startButtoncolor = (startButtoncolor[0], startButtoncolor[1] + 50, startButtoncolor[2])
@@ -58,6 +64,7 @@ def mainMenu(screen: pygame.Surface):
             # mouse button is down
             if mouseDown:
                 selection = "editor"
+                playSoundPitch(buttonClickSound)
             
             # increase the blue value of the editor button color
             editorButtonColor = (editorButtonColor[0], editorButtonColor[1], editorButtonColor[2] + 50)
@@ -68,6 +75,7 @@ def mainMenu(screen: pygame.Surface):
             # mouse button is down
             if mouseDown:
                 selection = "quit"
+                playSoundPitch(buttonClickSound)
             
             # increase the red value of the quit button color
             quitButtonColor = (quitButtonColor[0] + 50, quitButtonColor[1], quitButtonColor[2])
@@ -99,3 +107,48 @@ def mainMenu(screen: pygame.Surface):
         # check if the user has made a selection
         if selection != "none":
             return selection
+
+
+# returns a surface of the pause screen to be overlayed on the game screen, also returns a boolean value to indicate a button press
+def getPauseScreen(mx, my, mouseClick) -> tuple[pygame.Surface, str]:
+    selection = "none"
+
+    # create a surface for the pause screen
+    pauseScreen = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
+    # fill the surface with a black transparent color
+    pauseScreen.fill((0, 0, 0, 100))
+
+    # draw the text
+    pauseText = menuFont.render("PAUSED", False, (255, 255, 255))
+    pauseScreen.blit(pauseText, (WIDTH/2.65, HEIGHT/4))
+
+    # draw the resume button
+    resumeButton = pygame.Rect(WIDTH/2.5 + 150, HEIGHT/3, 100, 50)
+    pygame.draw.rect(pauseScreen, (0, 200, 0), resumeButton)
+    resumeText = menuButtonFont.render("Resume", False, (0, 0, 0))
+    pauseScreen.blit(resumeText, (resumeButton.x + (resumeButton.width - resumeText.get_width()) / 2, resumeButton.y + (resumeButton.height - resumeText.get_height()) / 2))
+
+    # draw the quit button
+    quitButton = pygame.Rect(WIDTH/2.5 + 150, HEIGHT/2 - 60, 100, 50)
+    pygame.draw.rect(pauseScreen, (200, 0, 0), quitButton)
+    quitText = menuButtonFont.render("Quit", False, (0, 0, 0))
+    pauseScreen.blit(quitText, (quitButton.x + (quitButton.width - quitText.get_width()) / 2, quitButton.y + (quitButton.height - quitText.get_height()) / 2))
+
+
+    ## check for button clicks ##
+    # check if the mouse is over the resume button
+    if mx > resumeButton.x and mx < resumeButton.x + resumeButton.width and my > resumeButton.y and my < resumeButton.y + resumeButton.height:
+        # mouse button is down
+        if mouseClick:
+            selection = "resume"
+            playSoundPitch(buttonClickSound)
+
+    if mx > quitButton.x and mx < quitButton.x + quitButton.width and my > quitButton.y and my < quitButton.y + quitButton.height:
+        # mouse button is down
+        if mouseClick:
+            selection = "quit"
+            playSoundPitch(buttonClickSound)
+
+    # return the surface
+    return pauseScreen, selection
+    
