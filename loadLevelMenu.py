6778b55e25f1fee29b1f1aa93fd828be8e1e5782
Owen Smith <owen.smith1201@gmail.com>
2023-08-30 01:18:14 -0400

    Controller support for level select menu

--- loadLevelMenu.py
+++ loadLevelMenu.py
@@ -96,6 +96,11 @@ def loadLevelMenu(screen: pygame.Surface, debug: bool = debug) -> tuple[list[Peg
     # get the names of each level in the levels folder
     levelsList = getLevelsList()
 
+    controllerConnected = False
+    controllerCursorIndex = 0
+    moveControllerSelector = None
+    controllerIndexRectPos = 1
+
     # if there are no levels in the levels folder, load the default level
     if len(levelsList) == 0:
         return loadDefaultLevel()
@@ -130,6 +135,9 @@ def loadLevelMenu(screen: pygame.Surface, debug: bool = debug) -> tuple[list[Peg
     # main loop
     while True:
         mouseDown = False
+        dpadDirection = None
+        backButtonPressed = False
+        selectButtonPressed = False
         # event handling
         for event in pygame.event.get():
             # quit if the user clicks the close button
@@ -148,24 +156,100 @@ def loadLevelMenu(screen: pygame.Surface, debug: bool = debug) -> tuple[list[Peg
                 if event.button == 5:
                     # scroll down
                     scrollValue -= 1
-                    pass
             
             # check if 1 is pressed (debug)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1:
                     debug = not debug
-        # update mouse posistion
-        mx, my = pygame.mouse.get_pos()
-        mousePos = Vector(mx, my)
+
+            # check for gamepad dpad buttons
+        if event.type == pygame.JOYHATMOTION:
+            if event.value == (0, 1):
+                dpadDirection = "up"
+            if event.value == (0, -1):
+                dpadDirection = "down"
+
+        if event.type == pygame.JOYBUTTONDOWN:
+            # if the controller is a 'sony' or 'playstation' controller (Sometimes "Wireless Controller" is the name of the ps4 controller, so we will include that as well)
+            if joystick.get_name().lower().find("sony") != -1 or joystick.get_name().lower().find("playstation") != -1 or joystick.get_name().lower().find("wireless") != -1:
+                if event.button == 0:  # the 'X'/cross button on a ps4 controller
+                    selectButtonPressed = True
+                if event.button == 1:  # the 'O'/circle button on a ps4 controller
+                    backButtonPressed = True
+                if event.button == 8:  # the 'share' button on a ps4 controller
+                    # enable or disable debug
+                    debug = not debug
+
+            else:  # xbox controller (default)
+                if event.button == 0:  # the 'A' button on an xbox controller
+                    selectButtonPressed = True
+                if event.button == 1:  # the 'B' button on an xbox controller
+                    backButtonPressed = True
+                if event.button == 6:  # the 'start' button on an xbox controller
+                    debug = not debug
+        
+
+        # check for joystick input
+        previousControllerSelector = moveControllerSelector
+        moveControllerSelector = None
+        if pygame.joystick.get_count() > 0:
+            # connect to the first controller
+            joystick = pygame.joystick.Joystick(0)
+            joystick.init()
+
+            # 1 is the Y axis on the left joystick (at least on xbox controllers)
+            joystickY = joystick.get_axis(1)
+
+            # check if the joystick is moved up or down
+            if joystickY < -0.5:
+                controllerConnected = True
+                moveControllerSelector = "up"
+            if joystickY > 0.5:
+                controllerConnected = True
+                moveControllerSelector = "down"
+        else:
+            # no controller connected
+            controllerConnected = False
+
+        if pygame.mouse.get_rel() != (0, 0):
+            controllerConnected = False
+            # unhide the mouse cursor
+            pygame.mouse.set_visible(True)
+
+        if controllerConnected:
+            # hide the mouse cursor
+            pygame.mouse.set_visible(False)
+
+            # move the controller selector
+            if (moveControllerSelector == "up" and previousControllerSelector is None) or dpadDirection == "up":
+                scrollValue += 1
+                controllerCursorIndex -= 1
+                # wrap around
+                if controllerCursorIndex < 0:
+                    controllerCursorIndex = len(levelsList)-1
+            if (moveControllerSelector == "down" and previousControllerSelector is None) or dpadDirection == "down":
+                scrollValue -= 1
+                controllerCursorIndex += 1
+                # wrap around
+                if controllerCursorIndex > len(levelsList)-1:
+                    controllerCursorIndex = 0
+        else:
+            # update mouse posistion
+            mx, my = pygame.mouse.get_pos()
+            mousePos = Vector(mx, my)
+
 
         ## check mouse input
         
         # check if mouse is over back button
-        if mousePos.x > backButtonPos.x and mousePos.x < backButtonPos.x + backButtonSize.x and mousePos.y > backButtonPos.y and mousePos.y < backButtonPos.y + backButtonSize.y:
-            # mouse button is down
-            if mouseDown:
-                selection = "mainMenu"
+        if not controllerConnected:
+            if mousePos.x > backButtonPos.x and mousePos.x < backButtonPos.x + backButtonSize.x and mousePos.y > backButtonPos.y and mousePos.y < backButtonPos.y + backButtonSize.y:
+                # mouse button is down
+                if mouseDown:
+                    selection = "mainMenu"
 
+        if backButtonPressed:
+            selection = "mainMenu"
 
         # draw the background
         screen.blit(altBackgroundImg, (0, 0))
@@ -213,26 +297,50 @@ def loadLevelMenu(screen: pygame.Surface, debug: bool = debug) -> tuple[list[Peg
             rect.x -= WIDTH/6
 
             # check if the mouse is over the level selection rectangle (apply scroll value)
-            if pygame.Rect(mousePos.x, mousePos.y - scrollValue*levelNameText.get_height(), 1, 1).colliderect(rect):
-                if mouseDown:
-                    selection = levelFilePath
-                    if debug:
-                        print ("Level selected: " + str(levelFilePath))
-                # mouse is hovering over the level selection rectangle
-                else:
+            if not controllerConnected:
+                if pygame.Rect(mousePos.x, mousePos.y - scrollValue*levelNameText.get_height(), 1, 1).colliderect(rect):
+                    if mouseDown:
+                        selection = levelFilePath
+                        if debug:
+                            print ("Level selected: " + str(levelFilePath))
+                    # mouse is hovering over the level selection rectangle
+                    else:
+                        color = (0, 255, 0)
+                        levelNameText = menuButtonFont.render(levelName, True, color)
+
+                        # blit the preview image at the top right corner of the screen
+                        screen.blit(levelPreviewImgs[i], (WIDTH - levelPreviewImgSizeW - 20, 20))
+            
+            # controller cursor
+            if controllerConnected:
+                if i == controllerCursorIndex:
                     color = (0, 255, 0)
                     levelNameText = menuButtonFont.render(levelName, True, color)
 
                     # blit the preview image at the top right corner of the screen
                     screen.blit(levelPreviewImgs[i], (WIDTH - levelPreviewImgSizeW - 20, 20))
 
+                    controllerIndexRectPos = rect.y
+
+
             # apply scroll value
             rect.y += scrollValue*levelNameText.get_height()
 
-            pygame.draw.rect(screen, color, rect, 2)
+            # only draw if the rect isn't off screen
+            if rect.y > -levelNameText.get_height() and rect.y < HEIGHT:
+                pygame.draw.rect(screen, color, rect, 2)
+
             
             # apply scroll value
             screen.blit(levelNameText, (WIDTH/5 +5 -WIDTH/6, HEIGHT/3 - levelNameText.get_height()/2 + i*levelNameText.get_height() + scrollValue*levelNameText.get_height()))
+
+
+        # if the controller select button is pressed
+        if selectButtonPressed:
+            selection = levelsList[controllerCursorIndex]
+            if debug:
+                print ("Level selected: " + str(levelsList[controllerCursorIndex]))
+            
 
         # draw the title (applying the scroll value)
         menuTitle = menuFont.render("Select a level", True, (255, 255, 255))
@@ -252,6 +360,12 @@ def loadLevelMenu(screen: pygame.Surface, debug: bool = debug) -> tuple[list[Peg
             framesPerSec = debugFont.render(
                 str(round(clock.get_fps())) + " fps", False, (255, 255, 255))
             screen.blit(framesPerSec, (5, 25))
+
+            # show controller info
+            if controllerConnected:
+                joystickName = debugFont.render(
+                    "Controller: " + joystick.get_name(), False, (255, 255, 255))
+                screen.blit(joystickName, (5, 40))
 
         # update display
         pygame.display.update()
